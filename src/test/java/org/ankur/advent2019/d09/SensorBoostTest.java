19b99f71272157f19f8a5c451d0f35ef7dff8542
Ankur Jain <ankur.jain@ing.com>
2019-12-17 16:49:47 +0100

    Day 9 using generic computer

--- src/test/java/org/ankur/advent2019/d09/SensorBoostTest.java
+++ src/test/java/org/ankur/advent2019/d09/SensorBoostTest.java
@@ -20,7 +20,7 @@ public class SensorBoostTest extends SensorBoost {
 
     @Test
     public void run() {
-        assertEquals(99, super.run("109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99", 1));
+        assertEquals(109, super.run("109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99", 1));
         long l = super.run("1102,34915192,34915192,7,4,7,99,0", 1);
         assertEquals(16, String.valueOf(l).length());
         assertEquals(1125899906842624L, super.run("104,1125899906842624,99", 1));
